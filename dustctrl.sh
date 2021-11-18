73b5878af245f2ec9f91ce94d0236401d10a5bee
carlos nikolaus <11008208+carlososvonknossos@users.noreply.github.com>
2021-11-18 23:39:02 +0100

    Update dustctrl.sh

--- dustctrl.sh
+++ dustctrl.sh
@@ -4,16 +4,16 @@ airctrl --ipaddr xxx.xxx.xxx.xxx --protocol coap --aqil 0
 while :
 do
 #var0=$(airctrl --ipaddr xxx.xxx.xxx.xxx --protocol coap | grep -o -P 'PM25:\ [0-254]' | cut -c 7-9)
-# trip point
+#### trip point: the WHO says https://www.who.int/news-room/fact-sheets/detail/ambient-(outdoor)-air-quality-and-health
 var1=7
 # grep values from https://github.com/opendata-stuttgart/sensors-software your fine dust sensor
-# PM2.5
+### PM2.5
 var2=$(curl -s http://xxx.xxx.xxx.xxx/values | grep -o -P '[0-9].[0-9]&nbsp;µg/m³' | cut -c -3 | sed -n 2p | awk '{print int($1+0.5)}' )
-# PM10
+### PM10
 var3=$(curl -s http://xxx.xxx.xxx.xxx/values | grep -o -P '[0-9].[0-9]&nbsp;µg/m³' | cut -c -3 | sed -n 1p | awk '{print int($1+0.5)}' )
-# Mix up 
+### Mix up 
 var4=$((var2+var3))
-# fan
+### fan
 fanspeed=$(airctrl --ipaddr xxx.xxx.xxx.xxx --protocol coap | grep -o -P 'Fan speed:\ [1,2,3,s,t,a]' | cut -c 12 ) &&
 #off=$( airctrl --ipaddr xxx.xxx.xxx.xxx --protocol coap --pwr 0 )
 #on=$(  airctrl --ipaddr xxx.xxx.xxx.xxx --protocol coap --pwr 1 )
